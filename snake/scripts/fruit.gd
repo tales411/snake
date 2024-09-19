26225831149c4c5738ea113ca6c87a2b324e9352
tales411 <tales.tatatdc@gmail.com>
2024-09-19 10:06:46 -0300

    frutas
    criação de frutas que aumentam o tamanho da cobra

--- snake/scripts/fruit.gd
+++ snake/scripts/fruit.gd
@@ -1,1 +1,6 @@
 extends Area2D
+
+
+func _on_body_entered(body) -> void:
+	if body.name == "player":
+		queue_free()
--- snake/scripts/main.gd
+++ snake/scripts/main.gd
@@ -5,7 +5,7 @@ func _ready():
 	Global.grid_size = 32 # Tamanho do grid 
 	Global.direction = Vector2.RIGHT # direção inicial (começa indo para a direita)
 	Global.snake_body = [] # lista do corpo da cobra
-	Global.initial_length = 10 # tamanho inicial da cobra
+	Global.initial_length = 2 # tamanho inicial da cobra
 	# cria o corpo inicial da cobra
 	for i in range(Global.initial_length):
 		var segment = Sprite2D.new()
@@ -23,13 +23,24 @@ func  _process(delta):
 			get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
-
-
 func _on_timer_timeout() -> void:
 	for i in range(Global.snake_body.size() - 1, 0, -1): # nao entendi o pq do - 1, 0, -1
 		Global.snake_body[i].position = Global.snake_body[i - 1].position
 	Global.snake_body[0].position = $player.position
 	
+func _grow():
+	print("aaa")
+	var new_segment = Sprite2D.new()
+	new_segment.texture = preload("res://tiles/barGreen_horizontalMid.png")
+	new_segment.scale.x = 1.6
+	new_segment.scale.y = 1.6
+	new_segment.position = Global.snake_body[Global.snake_body.size() - 1].position
+	add_child(new_segment)
+	Global.snake_body.append(new_segment)
 	
 	
-	
+
+func _on_fruit_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		_grow()
+		
