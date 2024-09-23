a8c7ff2ca6327ad2cb20ba8304d7daad45f09b5c
tales411 <tales.tatatdc@gmail.com>
2024-09-23 18:43:24 -0300

    pontuação e record

--- snake/scripts/main.gd
+++ snake/scripts/main.gd
@@ -2,13 +2,20 @@ extends  Node2D
 
 
 var fruit_scene = preload("res://scenes/fruit.tscn")
+var points = 0
 
+var record = 0
+var high_score = 0
+var config = ConfigFile.new()
 
 func _ready():
+	points = 0 # zera a pontuação no começo
 	Global.grid_size = 32 # Tamanho do grid 
 	Global.direction = Vector2.RIGHT # direção inicial (começa indo para a direita)
 	Global.snake_body = [] # lista do corpo da cobra
 	Global.initial_length = 5 # tamanho inicial da cobra
+	_load_record()
+	
 	# cria o corpo inicial da cobra
 	for i in range(Global.initial_length):
 		var segment = Sprite2D.new()
@@ -28,6 +35,7 @@ func  _process(delta):
 	if Global.fruit_entered == true:
 		_grow()
 		_spawn_fruit()
+		_score()
 		Global.fruit_entered = false
 
 func _on_timer_timeout() -> void:
@@ -44,26 +52,61 @@ func _grow():
 	add_child(new_segment)
 	Global.snake_body.append(new_segment)
 	
+				
+				
+func _check_position(new_position):
+	for i in range(Global.snake_body.size() - 1, -1, -1):
+		# Verifica se a nova posição coincide com a posição do corpo da cobra
+		if Global.snake_body[i].position == new_position:
+			print("Posição inválida. A posição está colidindo com o corpo da cobra.")
+			return false  # Retorna false se a posição colidir
+	for existing_fruit in $fruits.get_children():
+		if existing_fruit.position == new_position:
+			print("Posição inválida: em cima de outra fruta")
+			return false
 	
-
-
+	return true  # Retorna true se não houver colisão
+				
+				
 func _spawn_fruit():
 	var valid_position = false
 	var fruit = fruit_scene.instantiate()
 	var new_position
+	
 	while valid_position == false:
-		var random_x = randi() % int(22)*32
-		var random_y = randi() % int(19)*32
-		new_position = Vector2(random_x, 2*32)
-		for i in range(Global.snake_body.size() - 1, 0, -1):
-			if Global.snake_body[i].position == new_position:
-				print("teste")
-				valid_position = false
-				break
-			else:
-				valid_position = true
+		# Gera uma nova posição aleatória na linha fixa
+		var random_x = (randi() % int(22) * 32) - 16 + (32 * 2)# -16 para alinhar no centro
+		var random_y = (randi() % int(19) * 32) - 16 + (32 * 5)
+		new_position = Vector2(random_x, random_y)
+		
+		# Verifica se a posição gerada é válida
+		valid_position = _check_position(new_position)
 				
-				
-				
+	# Quando uma posição válida for encontrada, posiciona a fruta
 	fruit.position = new_position
 	$fruits.add_child(fruit)
+
+
+func _score():
+	points += 1
+	$score/points.text = str(points)
+	if points > record:
+		record = points
+		
+		_save_record()
+		
+
+
+func _save_record():
+	$score/record.text = str(record)
+	
+	config.set_value("scores", "high_scores", record)
+	var path = "user://save.cfg"
+	config.save(path)
+	print(record)
+	
+func _load_record():
+	var path = "user://save.cfg"
+	config.load(path)
+	record = config.get_value("scores", "high_scores", record)
+	$score/record.text = str(record)
